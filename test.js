a48b9318188d66198db3f6c5075038f285e813f3
whitae <35728340+whitae@users.noreply.github.com>
2021-04-20 23:08:02 +0800

    123

---
+++ test.js
@@ -0,0 +1,9 @@
+var person = {
+    // 属性
+    name: 'Tom',
+    // 方法
+    say: function () { }
+};
+person.say = function () {
+    console.log("hello world");
+};
---
+++ test.ts
@@ -0,0 +1,9 @@
+let person = {
+    // 属性
+    name:'Tom',
+    // 方法
+    say:function(){} //TODO 类型模板
+}
+person.say = function(){
+    console.log("hello world");
+}
